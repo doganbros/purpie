4090f038fcf99af513f223ef5cc3b451a8b3930f
Kusi Musah Hussein <kusimusah@gmail.com>
2021-07-04 19:16:18 +0300

    remove old server

--- server/api/utils/customErrors/meetingError.js
+++
@@ -1,7 +0,0 @@
-const { ApiError } = require('./baseError');
-
-exports.MEETING_NOT_FOUND = new ApiError({
-  message: "Can't find meeting with this id",
-  status: 400,
-  name: 'MEETING_NOT_FOUND',
-});
