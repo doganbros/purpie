afa98c495a6f9809a16f0b32597c8d5431125227
Fecri Kaan Ulubey <f.kaan93@gmail.com>
2021-07-17 12:06:12 +0300

    Initial private page layout implemented

--- src/components/layouts/PrivatePageLayout/PrivatePageLayout.tsx
+++ src/components/layouts/PrivatePageLayout/PrivatePageLayout.tsx
@@ -2,7 +2,6 @@ import {
   Avatar,
   Box,
   Button,
-  Grid,
   Heading,
   ResponsiveContext,
   Sidebar,
@@ -47,21 +46,22 @@ const PrivatePageLayout: FC<Props> = ({
   const size = useContext(ResponsiveContext);
 
   return (
-    <Grid
-      fill
-      rows={['auto', 'flex']}
-      gap={{ column: 'small' }}
-      columns={['auto', 'flex']}
-      areas={[
-        { name: 'sidebar', start: [0, 1], end: [0, 1] },
-        { name: 'main', start: [1, 1], end: [1, 1] },
-      ]}
+    <Box
+      height="100vh"
+      direction="row"
+      style={{
+        borderRadius: '45px',
+        boxShadow: '0px 30px 50px rgba(243, 111, 62, 0.15)',
+      }}
     >
       <Sidebar
-        gridArea="sidebar"
         background="brand"
-        height="100vh"
-        overflow="hidden"
+        width="228px"
+        style={{
+          borderRadius: '45px',
+          marginRight: -93,
+          paddingRight: 93,
+        }}
         header={
           <Box pad="small">
             <Button onClick={() => history.push('/')}>
@@ -80,25 +80,39 @@ const PrivatePageLayout: FC<Props> = ({
           <SidebarButton {...v} key={v.path} />
         ))}
       </Sidebar>
-      <Box gridArea="main" overflow="auto" height="100vh">
-        <Box justify="between" direction="row">
-          <Box flex={{ grow: 5 }}>
-            <Box
-              background={{ light: 'white', dark: 'dark-1' }}
-              style={{ position: 'sticky', top: 0 }}
-              height="150px"
-              margin={{ bottom: 'medium' }}
-            >
-              <Heading level="2">
-                {IconComponent && <IconComponent />} {title}
-              </Heading>
-            </Box>
-            <Box>{children}</Box>
+      <Box
+        flex
+        overflow="auto"
+        background={{ light: 'white', dark: 'dark-1' }}
+        pad="30px 45px 0 45px"
+        style={{ borderRadius: '45px 0 0 45px', gap: '45px' }}
+      >
+        <Box flex>
+          <Box
+            background={{ light: 'white', dark: 'dark-1' }}
+            style={{ position: 'sticky', top: 0 }}
+            height="150px"
+            margin={{ bottom: 'medium' }}
+          >
+            <Heading level="2">
+              {IconComponent && <IconComponent />} {title}
+            </Heading>
           </Box>
-          {!['small', 'xsmall'].includes(size) && RightSideItem}
+          <Box>{children}</Box>
         </Box>
       </Box>
-    </Grid>
+      {!['small', 'xsmall'].includes(size) && (
+        <Box
+          width="387px"
+          style={{
+            borderRadius: '45px',
+            boxShadow: '-5px 5px 30px rgba(61, 19, 141, 0.15)',
+          }}
+        >
+          {RightSideItem}
+        </Box>
+      )}
+    </Box>
   );
 };
 
