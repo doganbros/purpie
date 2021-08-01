01ec5d9856064cb394c914edcbdbd99d9dc811bc
Fecri Kaan Ulubey <f.kaan93@gmail.com>
2021-08-01 18:04:31 +0300

    ZoneSelector update

--- src/components/layouts/PrivatePageLayout/ZoneSelector.tsx
+++ src/components/layouts/PrivatePageLayout/ZoneSelector.tsx
@@ -1,21 +1,45 @@
-import { Text, Box } from 'grommet';
+import { Select, Box, Text } from 'grommet';
 import { Down } from 'grommet-icons';
 import React, { FC } from 'react';
 
 const ZoneSelector: FC = () => {
+  const [value, setValue] = React.useState('Doganbros');
+
   return (
-    <Box
-      margin="small"
-      background="#FFFFFF26"
-      round="xxsmall"
-      direction="row"
-      align="center"
-      justify="around"
-      pad="small"
+    <Select
+      options={['Doganbros', 'Zone 2', 'Zone 3']}
+      plain
+      value={value}
+      icon={false}
+      valueLabel={
+        <Box
+          direction="row"
+          align="center"
+          justify="around"
+          background="brand"
+          fill
+          pad="xsmall"
+          margin="xsmall"
+          round="xsmall"
+        >
+          <Text size="xsmall" color="white">
+            {value}
+          </Text>
+          <Down size="small" color="white" />
+        </Box>
+      }
+      onChange={({ option }) => setValue(option)}
     >
-      <Text size="xsmall">Doganbros</Text>
-      <Down size="small" color="white" />
-    </Box>
+      {(option) => {
+        return (
+          <Box pad="xsmall" background={option === value ? '#E4E9F2' : 'white'}>
+            <Text weight={option === value ? 'bold' : 'normal'} size="small">
+              {option}
+            </Text>
+          </Box>
+        );
+      }}
+    </Select>
   );
 };
 
