8209239bbccd6ab70e595d756d5c2bdd22aee08e
Fecri Kaan Ulubey <f.kaan93@gmail.com>
2021-08-13 18:02:12 +0300

    Divider separated

--- src/components/layouts/PrivatePageLayout/PrivatePageLayout/Desktop.tsx
+++ src/components/layouts/PrivatePageLayout/PrivatePageLayout/Desktop.tsx
@@ -5,6 +5,7 @@ import ExtendedBox from '../../../utils/ExtendedBox';
 import Logo from '../../../../assets/octopus-logo/logo-white.svg';
 import Sidebar from '../Sidebar';
 import ZoneSelector from '../ZoneSelector';
+import Divider from '../../../utils/Divider';
 
 interface Props {
   topComponent?: React.ReactNode;
@@ -88,8 +89,8 @@ const Desktop: FC<Props> = ({ children, rightComponent, topComponent }) => {
                 {topComponent}
               </ExtendedBox>
             </Box>
-            <Box fill="horizontal" height="3px" pad={{ right: 'large' }}>
-              <Box fill background="#E4E9F2" />
+            <Box fill="horizontal" pad={{ right: 'large' }}>
+              <Divider />
             </Box>
           </ExtendedBox>
         )}
---
+++ src/components/utils/Divider.tsx
@@ -0,0 +1,8 @@
+import { Box } from 'grommet';
+import React, { FC } from 'react';
+
+const Divider: FC = () => (
+  <Box height="3px" fill="horizontal" background="#E4E9F2" />
+);
+
+export default Divider;
