640548e87c6204a086857abac8976556f2ee7f23
Fecri Kaan Ulubey <f.kaan93@gmail.com>
2021-11-07 19:11:01 +0300

    video-player.css to scss

--- src/hooks/useVideoJS.tsx
+++ src/hooks/useVideoJS.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
-import '../scss/video-player.css';
+import '../scss/video-player.scss';
 
 const defaultOptions: videojs.PlayerOptions = {
   autoplay: false,
