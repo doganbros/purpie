8e9b6ef3695759fc67a9b3bb696eefad8d5ce2b0
Kusi Musah Hussein <kusimusah@gmail.com>
2021-12-13 14:41:43 +0300

    docker reconfig

--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-# Stop all servers and start the server as a daemon
-cd /home/ubuntu/octopus
-sudo yarn install
-sudo yarn build
-sudo yarn server:start
--- server/src/utils/utils.module.ts
+++ server/src/utils/utils.module.ts
@@ -1,12 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Client } from 'entities/Client.entity';
 import { User } from 'entities/User.entity';
 import { MattermostService } from './mattermost.service';
+import { UtilService } from './util.service';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [MattermostService],
+  imports: [TypeOrmModule.forFeature([User, Client])],
+  providers: [MattermostService, UtilService],
   exports: [MattermostService],
 })
 export class UtilsModule {}
