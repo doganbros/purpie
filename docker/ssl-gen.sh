2241fcc025e16a80843419c4076c24465e43a211
Kusi Musah Hussein <kusimusah@gmail.com>
2021-12-17 20:48:45 +0300

    ssl certificate direction changes

--- docker/ssl-gen.sh
+++ docker/ssl-gen.sh
@@ -5,18 +5,7 @@ source ../.env
 DOMAIN="$REACT_APP_CLIENT_HOST"
 
 
-echo "-------------------------------------------------------------------------"
-echo "This script will:"
-echo "- Need a working DNS record pointing to this machine(for domain ${DOMAIN})"
-echo "- Download certbot-auto from https://dl.eff.org to /usr/local/sbin"
-echo "- Install additional dependencies in order to request Let’s Encrypt certificate"
-echo "- If running with jetty serving web content, will stop Jitsi Videobridge"
-echo "- Configure and reload nginx or apache2, whichever is used"
-echo "- Configure the coturn server to use Let's Encrypt certificate and add required deploy hooks"
-echo "- Add command in weekly cron job to renew certificates regularly"
-echo ""
-echo "You need to agree to the ACME server's Subscriber Agreement (https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf) "
-echo "by providing an email address for important account notifications"
+echo "--------------SSL Certificate Generation Started-------------"
 ​
 echo -n "Enter your email and press [ENTER]: "
 read EMAIL
@@ -48,13 +37,6 @@ if [ ! -x "$(command -v certbot)" ] ; then
         exit 1
     fi
 fi
-​
-# CRON_FILE="/etc/cron.weekly/letsencrypt-renew"
-# if [ ! -d "/etc/cron.weekly" ] ; then
-#     mkdir "/etc/cron.weekly"
-# fi
-# echo "#!/bin/bash" > $CRON_FILE
-# echo "/usr/bin/certbot renew >> /var/log/le-renew.log" >> $CRON_FILE
 ​
 CERT_KEY="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
 CERT_CRT="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
