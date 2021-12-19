8ae9a0885f4297bdbfaaa064eb01094c980fe10f
= <=>
2021-12-19 13:40:09 +0000

    finalize scripts

--- docker/scripts/rtmp/finalize-script/octopus-rtmp.sh
+++ docker/scripts/rtmp/finalize-script/octopus-rtmp.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-if ! command -v unzip &> /dev/null
-then
-    echo "Installing Unzip"
-    apt-dpkg-wrap apt-get update
-    apt-dpkg-wrap apt-get install unzip
-fi
-
 if ! command -v aws &> /dev/null
 then
     echo "Installing AWS"
-    sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "./awscliv2.zip"
-    sudo unzip ./awscliv2.zip
-    chmod +x ./aws/install
-    ./aws/install
+    apt-get install awscli -y
 fi
-
 
 DATE="$(date)"
 
