95541f957b6bb154a70f83a1941e82024be23524
Doganbros <hello@doganbros.com>
2022-02-02 21:03:08 +0300

    code reviews

--- docker/scripts/rtmp/finalize-script/octopus-rtmp.sh
+++ docker/scripts/rtmp/finalize-script/octopus-rtmp.sh
@@ -13,94 +13,72 @@ echo "#################################### $DATE --- Reporting Event to Octopus 
 echo "1: $1 - 2: $2 - 3:$3 - 4: $4"  
 echo "$DATE --- Event Type: $EVENT_TYPE - User ID: $USER_ID - Video ID: $VIDEO_ID" 
 
-upload(){
-# Set credentials for aws cli
-aws configure set aws_access_key_id $AWS_ACCESS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET_KEY
-aws configure set default.region $AWS_DEFAULT_REGION
-# Upload to S3
-aws s3 sync /home/recordings/$FILE_ID s3://$S3_BUCKET_NAME/meeting-recordings/$FILE_ID/
+upload() {
+  # Set credentials for aws cli
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY
+  aws configure set aws_secret_access_key $AWS_SECRET_KEY
+  aws configure set default.region $AWS_DEFAULT_REGION
+  # Upload to S3
+  aws s3 sync /home/recordings/$FILE_ID s3://$S3_BUCKET_NAME/meeting-recordings/$FILE_ID/
+  S3_EXIT_CODE=$(echo $?)
+  if [[ ${S3_EXIT_CODE} == 0 ]]
+    echo "/home/recordings/$FILE_ID successfully synced with $S3_BUCKET_NAME/meeting-recordings/$FOLDER_NAME/" >>/tmp/octopus-rtmp.log
+  else
+    echo "/home/recordings/$FILE_ID failed to sync with $S3_BUCKET_NAME/meeting-recordings/$FOLDER_NAME/. Status Code: $S3_EXIT_CODE" >>/tmp/octopus-rtmp.log
+  fi
 }
 
-refresh_octopus(){
-  echo "$DATE --- Authentication Token Expired. Attempting to Use Refresh Token." 
-RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"refreshToken": "'"$REFRESH_TOKEN"'"}' ${OCTOPUS_URL}auth/client/refresh-token)
-STATUS_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
-if [[ ${STATUS_CODE} == 401 || ${STATUS_CODE} == 403 || ${STATUS_CODE} == 400 ]]
- then
-  echo "$DATE --- Refresh Token has expired. Logging in to Octopus..." 
-  RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"apiKey": "'"$OCTOPUS_API_KEY"'", "apiSecret": "'"$OCTOPUS_API_SECRET"'"}' ${OCTOPUS_URL}auth/client/login)
-  STATUS_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
-  if [ ${STATUS_CODE} == 401 ]; then
-   echo "$DATE --- Error While Logging In: Not Authorized" 
-  elif [ ${STATUS_CODE} == 400 ]; then
-   echo "$DATE --- Error While Logging In: Bad Request" 
+auth() {
+  echo "$DATE - Authentication Token Expired. Attempting to Use Refresh Token." >>/tmp/octopus-rtmp.log
+  RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"refreshToken": "'"$REFRESH_TOKEN"'"}' ${OCTOPUS_URL}auth/client/refresh-token)
+  AUTH_STATUS_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
+  if [[ ${AUTH_STATUS_CODE} == 200 ]]; then
+    echo "$DATE - Auth token successfully renewed" >>/tmp/octopus-rtmp.log
+  elif [[ ${AUTH_STATUS_CODE} == 401 || ${AUTH_STATUS_CODE} == 403 || ${AUTH_STATUS_CODE} == 400 ]]; then
+    echo "$DATE - Refresh Token has expired. Re-authing to Octopus..." >>/tmp/octopus-rtmp.log
+    RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"apiKey": "'"$API_KEY"'", "apiSecret": "'"$API_SECRET"'"}' ${OCTOPUS_URL}auth/client/login)
+    LOGIN_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
+    if [[ ${LOGIN_RETURN_CODE} == 200 ]]; then
+      echo "$DATE - Successfully logged into Octopus" >>/tmp/octopus-rtmp.log
+    else
+      echo "$DATE - Error while re-auth. Returned: $LOGIN_RETURN_CODE" >>/tmp/octopus-rtmp.log  
+    fi
   else
-  echo "$DATE --- Successfully Logged into Octopus. Receiving New Tokens..." 
-   AUTH_TOKEN=$(echo ${RESPONSE} | jq -r '.accessToken')
-   REFRESH_TOKEN=$(echo ${RESPONSE} | jq -r '.refreshToken')
-   echo "AUTH_TOKEN=$AUTH_TOKEN" > token.txt
-   echo "REFRESH_TOKEN=$REFRESH_TOKEN" >> token.txt
+    echo "$DATE - Authentication failed with the status code: $AUTH_STATUS_CODE." >>/tmp/octopus-rtmp.log
   fi
-else
-  echo "$DATE --- Refresh Successful. Receiving New Tokens..." 
   AUTH_TOKEN=$(echo ${RESPONSE} | jq -r '.accessToken')
   REFRESH_TOKEN=$(echo ${RESPONSE} | jq -r '.refreshToken')
-  echo "AUTH_TOKEN=$AUTH_TOKEN" > token.txt
-  echo "REFRESH_TOKEN=$REFRESH_TOKEN" >> token.txt
-fi
+  #Overwriting previous tokens
+  echo "AUTH_TOKEN=$AUTH_TOKEN" >token.txt
+  echo "REFRESH_TOKEN=$REFRESH_TOKEN" >>token.txt
+  #Logging new tokens
+  echo "$DATE - Auth Token: $AUTH_TOKEN" >>/tmp/octopus-rtmp.log
+  echo "$DATE - Refresh Token: $REFRESH_TOKEN" >>/tmp/octopus-rtmp.log
 }
 
-send_streaming_event(){
- HEADER="Bearer $AUTH_TOKEN"
- echo "Sending Streaming Event: slug: $VIDEO_ID and userID is: $USER_ID URL: $OCTOPUS_URL" 
- RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: $HEADER" -d '{"event": "'"$EVENT_TYPE"'", "mediaType": "video", "slug": "'"$VIDEO_ID"'", "userId": '$USER_ID'}' ${OCTOPUS_URL}stream/client/event)
- echo "Response from Streaming Event: $RESPONSE" 
- if [[ $RESPONSE != OK ]]; then
- STATUS_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
- fi
- if [[ ${STATUS_CODE} == 401 || ${STATUS_CODE} == 403 || ${STATUS_CODE} == 400 ]]; then
-   echo "$DATE --- Error While Sending Streaming Event Information: $STATUS_CODE" 
-   FINAL_STATUS_CODE=$STATUS_CODE
- fi
-}
-
-send_recording_event(){
- HEADER="Bearer $AUTH_TOKEN"
- echo "Sending Recording Event: id: $FILE_ID and filename is: $FILENAME URL: $OCTOPUS_URL"
- RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: $HEADER" -d '{"id": "'"$FILE_ID"'", "type": "meeting-recording", "fileName": "'"$FILENAME"'"}' ${OCTOPUS_URL}video/client/feedback)
- if [[ $RESPONSE != OK || $RESPONSE != Created ]]; then
- echo "Recording Event Sent: $RESPONSE" 
- STATUS_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
- fi
- if [[ ${STATUS_CODE} == 401 || ${STATUS_CODE} == 403 || ${STATUS_CODE} == 400 ]]; then
-   echo "$DATE --- Error While Sending Recording Event Information. STATUS CODE: $STATUS_CODE" 
-   FINAL_STATUS_CODE=$STATUS_CODE
- fi
-}
-
-echo "$EVENT_TYPE" 
-if [[ $EVENT_TYPE == record-done ]]; then
- upload
- send_recording_event
- if [[ ${FINAL_STATUS_CODE} == 401 || ${FINAL_STATUS_CODE} == 403 || ${FINAL_STATUS_CODE} == 400 ]]; then
-  refresh_octopus
-  send_recording_event
-  if [[ ${FINAL_STATUS_CODE} == 401 || ${FINAL_STATUS_CODE} == 403 || ${FINAL_STATUS_CODE} == 400 ]]; then
-   echo "$DATE --- Error While Sending Event Information. $STATUS_CODE" 
+send_event() {
+  HEADER="Bearer $AUTH_TOKEN"
+  if [[ $EVENT_TYPE == record-done ]]
+    upload
+    echo "$DATE - Sending Recording Event: id: $FILE_ID and filename is: $FILENAME" >>/tmp/octopus-rtmp.log
+    RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: $HEADER" -d '{"id": "'"$FILE_ID"'", "type": "meeting-recording", "fileName": "'"$FILENAME"'"}' ${OCTOPUS_URL}video/client/feedback)
   else
-   echo "$DATE --- Event Sent Successfully" 
+    echo "$DATE - Sending Streaming Event: slug: $VIDEO_ID and userID is: $USER_ID" >>/tmp/octopus-rtmp.log
+    RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: $HEADER" -d '{"event": "'"$EVENT_TYPE"'", "mediaType": "video", "slug": "'"$VIDEO_ID"'", "userId": '$USER_ID'}' ${OCTOPUS_URL}stream/client/event)
   fi
- fi
-else
- send_streaming_event
- if [[ ${FINAL_STATUS_CODE} == 401 || ${FINAL_STATUS_CODE} == 403 || ${FINAL_STATUS_CODE} == 400 ]]; then
-  refresh_octopus
-  send_streaming_event
-  if [[ ${FINAL_STATUS_CODE} == 401 || ${FINAL_STATUS_CODE} == 403 || ${FINAL_STATUS_CODE} == 400 ]]; then
-   echo "$DATE --- Error While Sending Event Information. $STATUS_CODE" 
+  SEND_EVENT_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
+  if [[ $RESPONSE == OK || $RESPONSE == Created ]]; then
+    echo "$DATE - Event successfully sent to Octopus with response: $SEND_EVENT_RETURN_CODE" >>/tmp/octopus-rtmp.log
   else
-   echo "$DATE --- Event Sent Successfully" 
+    if [[ $NUM_SEND_EVENT_TRIES -lt $MAX_EVENT_TRIES ]]; then
+      NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
+      echo "$DATE - Error while sending event. Retuned code: ${SEND_EVENT_RETURN_CODE}. Num of tries: $NUM_SEND_EVENT_TRIES." >>/tmp/octopus-rtmp.log
+      auth
+      send_event      
+    else
+      echo "$DATE - Error while sending event after trying $NUM_SEND_EVENT_TRIES times. Retuned code: ${SEND_EVENT_RETURN_CODE}" >>/tmp/octopus-rtmp.log
+    fi
   fi
- fi
-fi
+}
+
+send_event
--- external/jibri/octopus-finalize.sh
+++ external/jibri/octopus-finalize.sh
@@ -10,6 +10,8 @@ AWS_DEFAULT_REGION=$AWS_REGION
 S3_BUCKET_NAME=$AWS_BUCKET
 SEND_EVENT_RETURN_CODE=null
 AUTH_RETURN_CODE=null
+NUM_SEND_EVENT_TRIES=0
+MAX_SEND_EVENT_TRIES=3
 
 RECORDINGS_DIR=$1
 echo "$DATE - Starting to finalize meeting recording on directory: $1" >>/home/finalize/finalize.log
@@ -36,36 +38,41 @@ upload() {
   aws configure set default.region $AWS_DEFAULT_REGION
   # Upload to S3
   aws s3 sync $RECORDINGS_DIR s3://$S3_BUCKET_NAME/meeting-recordings/$FOLDER_NAME/
-  # TODO: Get the returned value from AWS CLI and inform Octopus about the upload
+  #TODO: CONFIRM THIS IS WORKING
+  S3_EXIT_CODE=$(echo $?)
+  if [[ ${S3_EXIT_CODE} == 0 ]]
+    echo "$RECORDINGS_DIR successfully synced with $S3_BUCKET_NAME/meeting-recordings/$FOLDER_NAME/" >>/home/finalize/finalize.log
+  else
+    echo "$RECORDINGS_DIR failed to sync with $S3_BUCKET_NAME/meeting-recordings/$FOLDER_NAME/. Status Code: $S3_EXIT_CODE" >>/home/finalize/finalize.log
+  fi
 }
 
 auth() {
   echo "$DATE - Authentication token expired. Attempting to use Refresh Token." >>/home/finalize/finalize.log
   RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"refreshToken": "'"$REFRESH_TOKEN"'"}' ${OCTOPUS_URL}auth/client/refresh-token)
   AUTH_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
-
-  #TODO: TRANSFORM THE LOGIC BELOW TO "IF VALID CASE ELSEIF 403 ELSE BLOCK"
-
-  if [[ ${AUTH_RETURN_CODE} == 401 || ${AUTH_RETURN_CODE} == 403 || ${AUTH_RETURN_CODE} == 400 ]]; then
-    echo "$DATE --- Refresh Token has expired. Re-authing to Octopus..." >>/home/finalize/finalize.log
+  if [[ ${AUTH_RETURN_CODE} == 200 ]]; then
+    echo "Auth token successfully renewed" >>/home/finalize/finalize.log
+  elif [[ ${AUTH_RETURN_CODE} == 401 || ${AUTH_RETURN_CODE} == 403 || ${AUTH_RETURN_CODE} == 400 ]]; then
+    echo "$DATE - Refresh Token has expired. Re-authing to Octopus..." >>/home/finalize/finalize.log
     RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -d '{"apiKey": "'"$API_KEY"'", "apiSecret": "'"$API_SECRET"'"}' ${OCTOPUS_URL}auth/client/login)
-    AUTH_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
-    if [ ${AUTH_RETURN_CODE} == 403 ]; then
-      echo "$DATE --- Successfully auth to Octopus." >>/home/finalize/finalize.log
-      AUTH_TOKEN=$(echo ${RESPONSE} | jq -r '.accessToken')
-      REFRESH_TOKEN=$(echo ${RESPONSE} | jq -r '.refreshToken')
-      echo "$DATE --- Got new Auth Token: $AUTH_TOKEN" >/home/finalize/token.txt
-      echo "$DATE --- Got new Refresh Token: $REFRESH_TOKEN" >>/home/finalize/token.txt
+    LOGIN_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
+    if [[ ${LOGIN_RETURN_CODE} == 200 ]]; then
+      echo "Successfully logged into Octopus" >>/home/finalize/finalize.log
     else
-      echo "$DATE --- Error while re-auth. Returned: $AUTH_RETURN_CODE" >>/home/finalize/finalize.log
+      echo "$DATE - Error while re-auth. Returned: $LOGIN_RETURN_CODE" >>/home/finalize/finalize.log  
     fi
   else
-    echo "$DATE - Successfully auth to Octopus." >>/home/finalize/finalize.log
-    AUTH_TOKEN=$(echo ${RESPONSE} | jq -r '.accessToken')
-    REFRESH_TOKEN=$(echo ${RESPONSE} | jq -r '.refreshToken')
-    echo "$DATE --- Got new Auth Token: $AUTH_TOKEN" >/home/finalize/token.txt
-    echo "$DATE --- Got new Refresh Token: $REFRESH_TOKEN" >>/home/finalize/token.txt
+    echo "$DATE - Authentication failed with the status code: $AUTH_RETURN_CODE." >>/home/finalize/finalize.log
   fi
+  AUTH_TOKEN=$(echo ${RESPONSE} | jq -r '.accessToken')
+  REFRESH_TOKEN=$(echo ${RESPONSE} | jq -r '.refreshToken')
+  #Overwriting previous tokens
+  echo "AUTH_TOKEN=$AUTH_TOKEN" >/home/finalize/token.txt
+  echo "REFRESH_TOKEN=$REFRESH_TOKEN" >>/home/finalize/token.txt
+  #Logging new tokens
+  echo "$DATE - Auth Token: $AUTH_TOKEN" >>/home/finalize/finalize.log
+  echo "$DATE - Refresh Token: $REFRESH_TOKEN" >>/home/finalize/finalize.log
 }
 
 send_event() {
@@ -73,30 +80,21 @@ send_event() {
   echo "$DATE - Sending event to Octopus. Payload data is id: $FOLDER_NAME fileName: $RECORDINGS" >>/home/finalize/finalize.log
   RESPONSE=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: $HEADER" -d '{"id": "'"$FOLDER_NAME"'", "type": "meeting-recording", "fileName": "'"$RECORDINGS"'"}' ${OCTOPUS_URL}video/client/feedback)
   echo "$DATE - Got send event response : $RESPONSE" >>/home/finalize/finalize.log
+  SEND_EVENT_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
   #TO DO: UPDATE OCTOPUS BACKEND TO SUCCESS AND FAIL AND UPDATE HERE ACCORDINGLY
   if [[ $RESPONSE == OK || $RESPONSE == Created ]]; then
-    echo "$DATE - Event sent to Octopus with response: $RESPONSE" >>/home/finalize/finalize.log
+    echo "$DATE - Event successfully sent to Octopus with response: $SEND_EVENT_RETURN_CODE" >>/home/finalize/finalize.log
   else
-    SEND_EVENT_RETURN_CODE=$(echo ${RESPONSE} | jq -r '.statusCode')
-    if [[ ${SEND_EVENT_RETURN_CODE} == 401 || ${SEND_EVENT_RETURN_CODE} == 403 || ${SEND_EVENT_RETURN_CODE} == 400 ]]; then
-      echo "$DATE --- Error while sending event. Retuned code: ${SEND_EVENT_RETURN_CODE}" >>/home/finalize/finalize.log
+    if [[ $NUM_SEND_EVENT_TRIES -lt $MAX_SEND_EVENT_TRIES ]]; then
+      NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
+      echo "$DATE - Error while sending event. Retuned code: ${SEND_EVENT_RETURN_CODE}. Num of tries: $NUM_SEND_EVENT_TRIES." >>/home/finalize/finalize.log
+      auth
+      send_event      
+    else
+      echo "$DATE - Error while sending event after trying $NUM_SEND_EVENT_TRIES times. Retuned code: ${SEND_EVENT_RETURN_CODE}" >>/home/finalize/finalize.log
     fi
   fi
 }
 
 upload
-echo "$DATE --- Uploading file:  $name is done." >>/home/finalize/finalize.log
 send_event
-echo "$DATE --- Final status  is: $SEND_EVENT_RETURN_CODE" >>/home/finalize/finalize.log
-if [ -z "$SEND_EVENT_RETURN_CODE" ]; then
-  echo "$DATE --- Event sent successfully" >>/home/finalize/finalize.log
-else
-  echo "$DATE --- Couldn't send event. Will re-auth." >>/home/finalize/finalize.log
-  auth
-  send_event
-  if [ -z "$SEND_EVENT_RETURN_CODE" ]; then
-    echo "$DATE --- Event sent successfully" >>/home/finalize/finalize.log
-  else
-    echo "$DATE --- Error while sending event. Returned code: $SEND_EVENT_RETURN_CODE" >>/home/finalize/finalize.log
-  fi
-fi
