27b65ad56205c1bf3c56d507fc4795db276dd1a8
Fecri Kaan Ulubey <f.kaan93@gmail.com>
2022-03-07 22:06:54 +0300

    No comments text

--- src/pages/Private/video/Comments/CommentList.tsx
+++ src/pages/Private/video/Comments/CommentList.tsx
@@ -35,29 +35,37 @@ const CommentList: FC<CommentsProps> = ({ postId }) => {
         Comments
       </Text>
       {user && <Input user={user} postId={postId} />}
-      <InfiniteScroll items={comments.data}>
-        {(item: typeof comments.data[0]) => (
-          <Stack key={item.id} interactiveChild="first">
-            <Box
-              pad={{ vertical: 'small', right: 'small', left: 'medium' }}
-              margin={{ vertical: 'small', left: 'medium' }}
-              elevation="peach"
-              round="small"
-            >
-              <Box pad={{ left: 'small' }} gap="small">
-                <CommentBase comment={item} postId={postId} />
-                {item.replyCount > 0 && (
-                  <Replies parentComment={item} postId={postId} />
-                )}
+      {comments.data.length === 0 ? (
+        <Box margin={{ vertical: 'small' }}>
+          <Text color="status-disabled">
+            No comments yet. Be the first one to comment!
+          </Text>
+        </Box>
+      ) : (
+        <InfiniteScroll items={comments.data}>
+          {(item: typeof comments.data[0]) => (
+            <Stack key={item.id} interactiveChild="first">
+              <Box
+                pad={{ vertical: 'small', right: 'small', left: 'medium' }}
+                margin={{ vertical: 'small', left: 'medium' }}
+                elevation="peach"
+                round="small"
+              >
+                <Box pad={{ left: 'small' }} gap="small">
+                  <CommentBase comment={item} postId={postId} />
+                  {item.replyCount > 0 && (
+                    <Replies parentComment={item} postId={postId} />
+                  )}
+                </Box>
               </Box>
-            </Box>
-            <InitialsAvatar
-              id={item.user.id}
-              value={`${item.user.firstName} ${item.user.lastName}`}
-            />
-          </Stack>
-        )}
-      </InfiniteScroll>
+              <InitialsAvatar
+                id={item.user.id}
+                value={`${item.user.firstName} ${item.user.lastName}`}
+              />
+            </Stack>
+          )}
+        </InfiniteScroll>
+      )}
     </Box>
   );
 };
