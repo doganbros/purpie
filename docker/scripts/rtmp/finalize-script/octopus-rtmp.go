c1c661c262356b93296ebda81d736d57cbb18b01
Kusi Musah Hussein <kusimusah@gmail.com>
2022-05-15 19:13:57 +0300

    use script for rtmp

--- docker/scripts/rtmp/finalize-script/octopus-rtmp.go
+++ docker/scripts/rtmp/finalize-script/octopus-rtmp.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println(os.Environ())
+	ioutil.WriteFile("/tmp/octopus-rtmp.log", []byte(strings.Join(os.Environ(), "\n")), 0644)
 }
