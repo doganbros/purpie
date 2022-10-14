33ac517d473f4f9d0b9a199346d8fffa103c6ab1
marici-mac <marici-mac@192.168.1.103>
2022-10-14 23:43:07 +0300

    404 and coming soon pages redesign

--- src/pages/Private/ComingSoon.tsx
+++ src/pages/Private/ComingSoon.tsx
@@ -1,24 +1,45 @@
-import { Box, Heading, Text } from 'grommet';
-import { Connect } from 'grommet-icons';
+import { Box, Heading, Image, Text } from 'grommet';
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import PrivatePageLayout from '../../components/layouts/PrivatePageLayout/PrivatePageLayout';
 import PublicPageLayout from '../../components/layouts/PublicPageLayout/PublicPageLayout';
 import { AppState } from '../../store/reducers/root.reducer';
+import ComingSoonImage from '../../assets/coming-soon.svg';
+import { useResponsive } from '../../hooks/useResponsive';
 
 const ComingSoon: FC = () => {
   const { isAuthenticated } = useSelector((state: AppState) => state.auth);
+  const size = useResponsive();
 
   const render = (
-    <Box align="center" fill justify="center">
-      <Box margin="20px">
-        <Connect size="xlarge" />
+    <Box
+      height={size === 'small' ? 'calc(100vh - 120px)' : '100vh'}
+      align="center"
+      fill="horizontal"
+      justify="center"
+    >
+      <Image
+        width={size === 'small' ? '240px' : '480px'}
+        src={ComingSoonImage}
+      />
+      <Heading
+        level="2"
+        color="dark"
+        margin={{ top: 'large', bottom: 'small' }}
+      >
+        Coming Soon
+      </Heading>
+      <Box width={{ max: 'medium' }}>
+        <Text
+          weight="normal"
+          size="small"
+          color="status-disabled"
+          textAlign="center"
+        >
+          Start registering new zones and following new channels. Please create
+          your first video content.
+        </Text>
       </Box>
-      <Heading>Coming Soon</Heading>
-      <Text>
-        Stay tuned. We are launching soon. We are working hard. We are almost
-        ready to launch.
-      </Text>
     </Box>
   );
 
--- src/pages/Private/NotFound.tsx
+++ src/pages/Private/NotFound.tsx
@@ -1,26 +1,44 @@
-import { Box, Heading, Text } from 'grommet';
-import { Halt } from 'grommet-icons';
+import { Box, Heading, Image, Text } from 'grommet';
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import PrivatePageLayout from '../../components/layouts/PrivatePageLayout/PrivatePageLayout';
 import PublicPageLayout from '../../components/layouts/PublicPageLayout/PublicPageLayout';
 import { AnchorLink } from '../../components/utils/AnchorLink';
 import { AppState } from '../../store/reducers/root.reducer';
+import { useResponsive } from '../../hooks/useResponsive';
+import NotFoundImage from '../../assets/404.svg';
 
 const NotFound: FC = () => {
   const { isAuthenticated } = useSelector((state: AppState) => state.auth);
+  const size = useResponsive();
 
   const render = (
-    <Box align="center" fill justify="center">
-      <Box margin="20px">
-        <Halt size="xlarge" />
+    <Box
+      height={size === 'small' ? 'calc(100vh - 120px)' : '100vh'}
+      align="center"
+      fill="horizontal"
+      justify="center"
+    >
+      <Image width={size === 'small' ? '240px' : '480px'} src={NotFoundImage} />
+      <Heading
+        level="2"
+        color="dark"
+        margin={{ top: 'large', bottom: 'small' }}
+      >
+        Page not found
+      </Heading>
+      <Box width={{ max: 'medium' }}>
+        <Text
+          weight="normal"
+          size="small"
+          color="status-disabled"
+          textAlign="center"
+        >
+          It looks like you may have taken a wrong turn. Do not worry... It
+          happens to the best of us.
+        </Text>
       </Box>
-      <Heading>Page not found</Heading>
-      <Text>
-        It looks like you may have taken a wrong turn. Do not worry... it
-        happens to the best of us.
-      </Text>
-      <Box margin="20px">
+      <Box margin={{ top: 'small' }}>
         {isAuthenticated ? (
           <AnchorLink label="Zones" to="/" />
         ) : (
