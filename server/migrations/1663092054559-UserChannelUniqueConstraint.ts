1cdb4c6ed62e2354eae26fdc6555d0263ae153ac
Suleyman <suleyman.sahin@orema.com.tr>
2022-10-23 00:56:20 +0300

    Unique index added for UserChannels. Migration added

---
+++ server/migrations/1663092054559-UserChannelUniqueConstraint.ts
@@ -0,0 +1,29 @@
+import {MigrationInterface, QueryRunner, TableUnique} from 'typeorm';
+
+export class UserChannelUniqueConstraint1663092054558 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // Remove duplicated rows
+        await queryRunner.query(`
+            DELETE
+                FROM user_channel
+                WHERE ID NOT IN
+                      (SELECT MAX(ID) AS MaxRecordID
+                       FROM user_channel
+                       GROUP BY "userId", "channelId", "channelRoleCode");
+        `);
+
+
+        const constraint = new TableUnique({
+            name: 'user_channel_unique_index',
+            columnNames: ['userId', 'channelId', 'channelRoleCode']
+        });
+
+        await queryRunner.createUniqueConstraint(
+            'user_channel',
+            constraint
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+}
