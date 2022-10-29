b1f3f336bbde02dfda20dd79f70f46ef4255abde
marici <mehmetaricistd@gmail.com>
2022-10-29 14:39:30 +0300

    auth button padding fix and ellipses text fix

--- src/components/utils/EllipsesOverflowText.tsx
+++ src/components/utils/EllipsesOverflowText.tsx
@@ -4,13 +4,12 @@ import styled from 'styled-components';
 
 interface Props extends TextProps {
   maxWidth?: string;
-  lineClamp?: number;
 }
 
 const EllipsesOverflowTextStyled = styled(Text)`
   display: -webkit-box;
   max-width: ${(props: Props) => props.maxWidth || '120px'};
-  -webkit-line-clamp: ${(props: Props) => props.lineClamp || 2};
+  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
   text-overflow: ellipsis;
