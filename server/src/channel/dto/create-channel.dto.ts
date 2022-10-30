045878709b7a5b2df3364b2fca1a1e333d43d642
marici <mehmetaricistd@gmail.com>
2022-10-30 23:44:58 +0300

    lint fix

--- server/src/channel/dto/create-channel.dto.ts
+++ server/src/channel/dto/create-channel.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsBoolean,
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateChannelDto {
