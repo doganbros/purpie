e1d1ef1ddd4b13e39949c480ecb167ce327d75e5
Hicabi YILMAZ <45284106+hcbylmz@users.noreply.github.com>
2023-01-09 20:18:26 +0300

    Avatar Upload redesign

--- src/pages/Private/settings/AvatarUpload.tsx
+++ src/pages/Private/settings/AvatarUpload.tsx
@@ -10,6 +10,7 @@ import {
   Text,
 } from 'grommet';
 import { useTranslation } from 'react-i18next';
+import { Close } from 'grommet-icons';
 import { apiURL } from '../../../config/http';
 
 interface AvatarUploadProps {
@@ -30,85 +31,78 @@ const AvatarUpload: FC<AvatarUploadProps> = ({
 
   return (
     <Layer responsive={false}>
-      <Box
-        width="medium"
-        height="medium"
-        pad="large"
-        margin="small"
-        justify="between"
-        align="center"
-        gap="small"
-      >
-        <Form
-          onSubmit={({ value }: any) => {
-            onSubmit({ photoFile: value.photoFile[0] });
-          }}
-        >
-          <Box
-            width="medium"
-            height="small"
-            alignSelf="center"
-            justify="center"
+      <Box elevation="peach" round="20px">
+        <Box pad="small" justify="between" direction="row">
+          <Text size="small">{t('settings.chooseProfilePicture')}</Text>
+          <Close color="brand" size="18px" onClick={onDismiss} />
+        </Box>
+
+        <Box margin={{ horizontal: 'small', bottom: 'small' }}>
+          <Form
+            onSubmit={({ value }: any) => {
+              onSubmit({ photoFile: value.photoFile[0] });
+            }}
           >
-            {imgSrc ? (
-              <Image fit="contain" src={imgSrc} />
-            ) : (
-              src !== null && (
-                <Image
-                  fit="contain"
-                  src={`${apiURL}/${type}/display-photo/${src}`}
-                />
-              )
-            )}
-          </Box>
-          <FormField name="photoFile" htmlFor="file-input">
-            <FileInput
-              name="photoFile"
-              id="photoFile"
-              multiple={false}
-              type="file"
-              accept="image/jpg,
-          image/jpeg,
-          image/png,
-          image/bmp,
-          image/svg+xml"
-              renderFile={(file) => {
-                <Box width="small" height="small">
-                  <Text>{file.name}</Text>
-                </Box>;
-              }}
-              onChange={(file) => {
-                if (file.target.files && file.target.files.length > 0) {
-                  const reader = new FileReader();
-                  reader.addEventListener('load', () =>
-                    setImgSrc(reader?.result?.toString() || '')
-                  );
-                  reader.readAsDataURL(file.target.files[0]);
-                }
-              }}
-            />
-          </FormField>
-          <Box justify="center" direction="row" gap="small">
-            <Button
-              label={t('settings.cancel')}
-              style={{ borderRadius: '10px' }}
-              size="large"
-              fill="horizontal"
-              margin={{ top: 'medium' }}
-              onClick={onDismiss}
-            />
-            <Button
-              label={t('settings.submit')}
-              type="submit"
-              primary
-              style={{ borderRadius: '10px' }}
-              size="large"
-              fill="horizontal"
-              margin={{ top: 'medium' }}
-              disabled={!imgSrc}
-            />
-          </Box>
-        </Form>
+            <Box
+              width={{ min: '276px', max: '276px' }}
+              height={{ min: '276px', max: '276px' }}
+              alignSelf="center"
+              margin={{ bottom: 'small' }}
+              round="20px"
+              overflow="hidden"
+            >
+              {imgSrc ? (
+                <Image fit="cover" src={imgSrc} />
+              ) : (
+                src !== null && (
+                  <Image
+                    fit="cover"
+                    src={`${apiURL}/${type}/display-photo/${src}`}
+                  />
+                )
+              )}
+            </Box>
+
+            <FormField name="photoFile" htmlFor="file-input">
+              <FileInput
+                name="photoFile"
+                id="photoFile"
+                multiple={false}
+                type="file"
+                accept="image/jpg,
+                        image/jpeg,
+                        image/png,
+                        image/bmp,
+                        image/svg+xml"
+                renderFile={(file) => {
+                  <Box width="small" height="small">
+                    <Text>{file.name}</Text>
+                  </Box>;
+                }}
+                onChange={(file) => {
+                  if (file.target.files && file.target.files.length > 0) {
+                    const reader = new FileReader();
+                    reader.addEventListener('load', () =>
+                      setImgSrc(reader?.result?.toString() || '')
+                    );
+                    reader.readAsDataURL(file.target.files[0]);
+                  }
+                }}
+              />
+            </FormField>
+
+            <Box justify="center" direction="row" gap="small">
+              <Button
+                label={t('settings.submit')}
+                type="submit"
+                primary
+                size="large"
+                fill="horizontal"
+                disabled={!imgSrc}
+              />
+            </Box>
+          </Form>
+        </Box>
       </Box>
     </Layer>
   );
