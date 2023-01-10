590bb7d245866d36ba15e6e0844e872d294b5e09
Hicabi YILMAZ <45284106+hcbylmz@users.noreply.github.com>
2023-01-10 21:08:25 +0300

    AvatarUpload changes made

--- src/pages/Private/settings/AvatarUpload.tsx
+++ src/pages/Private/settings/AvatarUpload.tsx
@@ -8,10 +8,12 @@ import {
   Image,
   Layer,
   Text,
+  ThemeContext,
 } from 'grommet';
 import { useTranslation } from 'react-i18next';
 import { Close } from 'grommet-icons';
 import { apiURL } from '../../../config/http';
+import { theme } from '../../../config/app-config';
 
 interface AvatarUploadProps {
   onSubmit: any | ((arg0: File) => void);
@@ -30,81 +32,98 @@ const AvatarUpload: FC<AvatarUploadProps> = ({
   const { t } = useTranslation();
 
   return (
-    <Layer responsive={false}>
-      <Box elevation="peach" round="20px">
-        <Box pad="small" justify="between" direction="row">
-          <Text size="small">{t('settings.chooseProfilePicture')}</Text>
-          <Close color="brand" size="18px" onClick={onDismiss} />
-        </Box>
+    <ThemeContext.Extend
+      value={{
+        ...theme,
+        formField: {
+          border: {
+            color: 'status-disabled',
+          },
+          round: true,
+        },
+        fileInput: {
+          border: {
+            size: '1px',
+            style: 'solid',
+          },
+          message: {
+            size: 'small',
+          },
+        },
+      }}
+    >
+      <Layer responsive={false}>
+        <Box elevation="peach" round="20px">
+          <Box pad="small" justify="between" direction="row">
+            <Text size="small">{t('settings.chooseProfilePicture')}</Text>
+            <Close color="brand" size="18px" onClick={onDismiss} />
+          </Box>
 
-        <Box margin={{ horizontal: 'small', bottom: 'small' }}>
-          <Form
-            onSubmit={({ value }: any) => {
-              onSubmit({ photoFile: value.photoFile[0] });
-            }}
-          >
-            <Box
-              width={{ min: '276px', max: '276px' }}
-              height={{ min: '276px', max: '276px' }}
-              alignSelf="center"
-              margin={{ bottom: 'small' }}
-              round="20px"
-              overflow="hidden"
+          <Box margin={{ horizontal: 'small', bottom: 'small' }}>
+            <Form
+              onSubmit={({ value }: any) => {
+                onSubmit({ photoFile: value.photoFile[0] });
+              }}
             >
-              {imgSrc ? (
-                <Image fit="cover" src={imgSrc} />
-              ) : (
-                src !== null && (
-                  <Image
-                    fit="cover"
-                    src={`${apiURL}/${type}/display-photo/${src}`}
-                  />
-                )
-              )}
-            </Box>
+              <Box
+                width={{ min: '276px', max: '276px' }}
+                height={{ min: '276px', max: '276px' }}
+                alignSelf="center"
+                margin={{ bottom: 'small' }}
+                round="20px"
+                overflow="hidden"
+              >
+                {imgSrc ? (
+                  <Image fit="cover" src={imgSrc} />
+                ) : (
+                  src !== null && (
+                    <Image
+                      fit="cover"
+                      src={`${apiURL}/${type}/display-photo/${src}`}
+                    />
+                  )
+                )}
+              </Box>
 
-            <FormField name="photoFile" htmlFor="file-input">
-              <FileInput
-                name="photoFile"
-                id="photoFile"
-                multiple={false}
-                type="file"
-                accept="image/jpg,
+              <FormField name="photoFile" htmlFor="file-input">
+                <FileInput
+                  messages={{ browse: t('settings.browse') }}
+                  name="photoFile"
+                  id="photoFile"
+                  multiple={false}
+                  type="file"
+                  accept="image/jpg,
                         image/jpeg,
                         image/png,
                         image/bmp,
                         image/svg+xml"
-                renderFile={(file) => {
-                  <Box width="small" height="small">
-                    <Text>{file.name}</Text>
-                  </Box>;
-                }}
-                onChange={(file) => {
-                  if (file.target.files && file.target.files.length > 0) {
-                    const reader = new FileReader();
-                    reader.addEventListener('load', () =>
-                      setImgSrc(reader?.result?.toString() || '')
-                    );
-                    reader.readAsDataURL(file.target.files[0]);
-                  }
-                }}
-              />
-            </FormField>
+                  onChange={(file) => {
+                    if (file.target.files && file.target.files.length > 0) {
+                      const reader = new FileReader();
+                      reader.addEventListener('load', () =>
+                        setImgSrc(reader?.result?.toString() || '')
+                      );
+                      reader.readAsDataURL(file.target.files[0]);
+                    }
+                  }}
+                />
+              </FormField>
 
-            <Box justify="center" direction="row" gap="small">
-              <Button
-                label={t('settings.submit')}
-                type="submit"
-                primary
-                size="large"
-                fill="horizontal"
-                disabled={!imgSrc}
-              />
-            </Box>
-          </Form>
+              <Box justify="center" direction="row" gap="small">
+                <Button
+                  label={<Text size="small">{t('settings.submit')}</Text>}
+                  type="submit"
+                  primary
+                  size="large"
+                  fill="horizontal"
+                  disabled={!imgSrc}
+                />
+              </Box>
+            </Form>
+          </Box>
         </Box>
-      </Box>
-    </Layer>
+      </Layer>
+    </ThemeContext.Extend>
   );
 };
 
