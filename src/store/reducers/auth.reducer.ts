4a6318fd76215a8466053e3e37025bdd6a35d984
marici <mehmetaricistd@gmail.com>
2023-02-23 23:22:00 +0300

    persist redux removed from localStorage after logout

--- src/store/reducers/auth.reducer.ts
+++ src/store/reducers/auth.reducer.ts
@@ -281,12 +281,14 @@ const authReducer = (
           loading: false,
         },
       };
-    case LOGOUT:
+    case LOGOUT: {
+      localStorage.removeItem('persist:root'); // remove all persisted data from localStorage for redux
       return {
         ...state,
         isAuthenticated: false,
         user: null,
       };
+    }
     case INITIALIZE_USER_REQUESTED:
       return {
         ...state,
